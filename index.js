c4061b71a1efe431591183a0afc575c073ec82fd
Franciel Mesquita <francielmsq@gmail.com>
2024-03-10 18:05:59 -0300

    ugws

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express();
+
+
+    app.use("/",express.static(path.join(__dirname,'public'))) //referencia a pasta public onde esta o index.html
+
+
+app.listen(3000,()=>{
+    console.log("rodando servidr")
+})
