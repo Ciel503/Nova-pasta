1ed5f7b47fe309c8787c31d7b588314827dedf2c
Franciel Mesquita <106025802+Ciel503@users.noreply.github.com>
2024-03-12 15:38:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const app = express();
     app.use("/",express.static(path.join(__dirname,'public'))) //referencia a pasta public onde esta o index.html
 
 
-app.listen(3000,()=>{
+app.listen(80,()=>{
     console.log("rodando servidr")
 })
